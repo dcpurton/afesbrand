f3e5a5d20e67a4c606bb3e28ed57abd4cf0cea90
David Purton <dcpurton@marshwiggle.net>
2026-04-13 23:11:26 +0930

    Add tagging code to build.lua

--- build.lua
+++ build.lua
@@ -11,3 +11,58 @@ typesetexe = "lualatex"
 
 stdengine    = "luatex"
 checkengines = {"luatex"}
+
+tagfiles = {"*.def", "*.sty", "*.tex", "README.md"}
+
+function update_tag(file, content, tagname, tagdate)
+  -- Update version in LaTeX package/class
+  content = content:gsub(
+    "(\\Provides%w+%s*%b{}%s*%[)%d%d%d%d%-%d%d%-%d%d%s+v[%d%.%a]+",
+    "%1" .. tagdate .. " v" .. tagname
+  )
+
+  -- Update version in Expl package/class
+  content = content:gsub(
+    "(\\ProvidesExpl%w+%s*%b{}%s*)(%b{})(%s*)(%b{})",
+    function(prefix, _date, ws, _ver)
+      return prefix
+        .. "{" .. tagdate .. "}"
+        .. ws
+        .. "{" .. tagname .. "}"
+    end
+  )
+
+  -- Update version in documentation
+  content = content:gsub(
+    "(\\date%{)%d%d%d%d[%-%/]%d%d[%-%/]%d%d%s+v[%d%.%a]+(%})",
+    "%1" .. tagdate .. " v" .. tagname .. "%2"
+  )
+
+  -- Update end year in copyright range: Copyright (c) YYYY-YYYY
+  content = content:gsub(
+    "(Copyright %([Cc]%) %d%d%d%d%-)(%d%d%d%d)",
+    function(prefix, endyear)
+      local tagyear = tagdate:sub(1, 4)
+      if tagyear > endyear then
+        return prefix .. tagyear
+      else
+        return prefix .. endyear
+      end
+    end
+  )
+
+  -- Update single copyright year: if tagdate year is greater, convert to range
+  content = content:gsub(
+    "(Copyright %([Cc]%) )(%d%d%d%d)([^%-%d])",
+    function(prefix, year, after)
+      local tagyear = tagdate:sub(1, 4)
+      if tagyear > year then
+        return prefix .. year .. "-" .. tagyear .. after
+      else
+        return prefix .. year .. after
+      end
+    end
+  )
+
+  return content
+end
